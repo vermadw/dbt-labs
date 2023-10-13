aba3c0315b41d5a59d44c6c039bbc159a35d4723
Mike Alfare <mike.alfare@dbtlabs.com>
2023-10-12 20:47:49 -0400

    add comments explaining how postgres last refreshed date works for materialized views

--- tests/functional/materializations/materialized_view_tests/files.py
+++ tests/functional/materializations/materialized_view_tests/files.py
@@ -6,6 +6,8 @@ id,value
 """.strip()
 
 
+# postgres does not let you inspect the database for a last refresh date
+# we need to create our own by adding a last_modified date to materialized view itself
 MODEL__MY_MATERIALIZED_VIEW = """
 {{ config(
     materialized="materialized_view"
@@ -14,6 +16,7 @@ select *, now() as last_refreshed from {{ ref('my_seed') }}
 """
 
 
+# see above for why we are just querying the table instead of a metadata table
 MACRO__LAST_REFRESH = """
 {% macro postgres__test__last_refresh(schema, identifier) %}
     {% set _sql %}
