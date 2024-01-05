e4ddbc8114c7f242f8a2050b957dfd79288e7a2c
Michelle Ark <michelle.ark@dbtlabs.com>
2024-01-05 14:24:07 -0500

    replace dbt/common with dbt-common

--- core/setup.py
+++ core/setup.py
@@ -74,6 +74,7 @@ setup(
         "dbt-extractor~=0.5.0",
         "minimal-snowplow-tracker~=0.0.2",
         "dbt-semantic-interfaces~=0.5.0a2",
+        "dbt-common @ git+https://github.com/dbt-labs/dbt-common.git@copy-code-from-dbt-core#egg=dbt",
         # ----
         # Expect compatibility with all new versions of these packages, so lower bounds only.
         "jsonschema>=3.0",
