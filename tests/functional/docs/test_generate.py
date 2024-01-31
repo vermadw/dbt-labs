cd3c2d2d89bc1038fff5220e688d9918168f4278
Michelle Ark <MichelleArk@users.noreply.github.com>
2024-01-31 12:55:46 -0500

    TestGenerateCatalogWithExternalNodes (#9457)

--- tests/functional/docs/test_generate.py
+++ tests/functional/docs/test_generate.py
@@ -1,5 +1,7 @@
 import pytest
+from unittest import mock
 
+from dbt.plugins.manifest import PluginNodes, ModelNodeArgs
 from dbt.tests.util import run_dbt, get_manifest
 import json
 
@@ -25,3 +27,25 @@ class TestGenerate:
         assert catalog["nodes"] == {}, "nodes should be empty"
         assert catalog["sources"] == {}, "sources should be empty"
         assert catalog["errors"] is None, "errors should be null"
+
+
+class TestGenerateCatalogWithExternalNodes(TestGenerate):
+    @mock.patch("dbt.plugins.get_plugin_manager")
+    def test_catalog_with_sources(self, get_plugin_manager, project):
+        project.run_sql("create table {}.external_model (id int)".format(project.test_schema))
+
+        run_dbt(["build"])
+
+        external_nodes = PluginNodes()
+        external_model_node = ModelNodeArgs(
+            name="external_model",
+            package_name="external_package",
+            identifier="external_model",
+            schema=project.test_schema,
+            database="dbt",
+        )
+        external_nodes.add_model(external_model_node)
+        get_plugin_manager.return_value.get_nodes.return_value = external_nodes
+        catalog = run_dbt(["docs", "generate"])
+
+        assert "model.external_package.external_model" in catalog.nodes
